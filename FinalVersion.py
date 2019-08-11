3d759fe3a4d45e77b176fa2c98f9479311d066fa
lyr17139789 <53497641+lyr17139789@users.noreply.github.com>
2019-08-10 22:09:59 -0700

    Add files via upload

--- FinalVersion.py
+++ FinalVersion.py
@@ -1,6 +1,17 @@
 import os
+os.system("sudo apt-get install vim")
+os.system("sudo apt install apache2 mysql-client mysql-server php libapache2-mod-php")
+
+os.system("sudo apt install graphviz aspell ghostscript clamav php7.2-pspell php7.2-curl php7.2-gd php7.2-intl php7.2-mysql php7.2-xml php7.2-xmlrpc php7.2-ldap php7.2-zip php7.2-soap php7.2-mbstring")
+print("Install Additional Software finished")
+
+os.system("sudo service apache2 restart")
+
+os.system("sudo apt install git")
+print("git down;oad finished")
+
 os.system("sudo apt-get install python3-pip")
-os.systm("sudo pip3 install --upgrade pip")
+os.system("sudo pip3 install --upgrade pip")
 os.system("sudo pip3 install pymysql")
 import pymysql
 #import csv
