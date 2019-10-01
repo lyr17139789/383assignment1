dfd6bad4c90f746e0deab49d52986cb99c61ef4a
lyr1719789 <44421919+lyr1719789@users.noreply.github.com>
2019-10-01 20:03:53 +1300

    Add files via upload

---
+++ manual.py
@@ -0,0 +1,4 @@
+global static_ip,host_ip,sql_name
+static_ip = input("your instance static external ip:")
+host_ip = input("your google cloud sql host ip :")
+sql_name = input("your google cloud sql instance name is:")
